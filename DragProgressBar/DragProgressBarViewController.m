58193016798aa2966528f719d14ab74a10976e74
ZQ0815 <zhangqiang9434@qq.com>
2020-11-24 15:40:55 +0800

    feat: 添加定时器功能刷新ui

--- DragProgressBar/DragProgressBarViewController.m
+++ DragProgressBar/DragProgressBarViewController.m
@@ -12,6 +12,10 @@
 
 @interface DragProgressBarViewController ()
 
+@property (nonatomic, strong) NSTimer *timer;
+@property (nonatomic, strong) SLProgressView *slProgressView;
+@property (nonatomic, assign) CGFloat progress;
+
 @end
 
 @implementation DragProgressBarViewController
@@ -21,17 +25,55 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
-    // 主view
     UIView *mainView = [UIView new];
     mainView.frame = CGRectMake(0, self.view.frame.size.height / 2 - 100 - navHeight, self.view.frame.size.width, 200);
     mainView.backgroundColor = [UIColor grayColor];
     
-    // 直线进度条
-    SLProgressView *SLProgress = [[SLProgressView alloc] initWithFrame:CGRectMake(0, 95, 200, 10)];
-    [SLProgress setProgress:11];
-    
-    [mainView addSubview:SLProgress];
+    [mainView addSubview:self.slProgressView];
     [self.view addSubview:mainView];
+    
+    [self.slProgressView setProgress:self.progress];
+    [self addTimer];
+}
+
+- (SLProgressView *)slProgressView {
+    if (!_slProgressView) {
+        _slProgressView = [[SLProgressView alloc] initWithFrame:CGRectMake(0, 95, 200, 10)];
+    }
+    return _slProgressView;
+}
+
+- (CGFloat)progress {
+    if (!_progress) {
+        _progress = 0.5;
+    }
+    return _progress;
+}
+
+#pragma mark - Timer
+
+- (void)addTimer {
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(refalshProgress:) userInfo:nil repeats:YES];
+}
+
+
+- (void)removeTimer
+{
+    [_timer invalidate];
+    _timer = nil;
+}
+
+#pragma mark - userMethods
+
+- (void)refalshProgress:(NSTimer *)theTimer {
+    if (_progress < 1) {
+        _progress += 0.05;                   
+        [self.slProgressView setProgress:self.progress];
+    }
+    else {
+        [self removeTimer];
+        _timer = nil;
+    }
 }
 
 @end
